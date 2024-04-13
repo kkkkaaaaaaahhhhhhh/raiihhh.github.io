9551911c52a1874b442164bc84977527073052f5
kkkkaaaaaaahhhhhhh <166179658+kkkkaaaaaaahhhhhhh@users.noreply.github.com>
2024-04-12 21:54:16 -0300

    upload

---
+++ questions.js
@@ -0,0 +1,50 @@
+export default [
+    {
+        question: "Que dia nos conhecemos?",
+        answers: [
+            {option: "12/01/2022", correct: false },
+            {option: "16/04/2023", correct: false },
+            {option: "12/01/2023", correct: true },
+        ],
+    },
+    {
+        question: "Qual foi a minha primeira mensagem para você?",
+        answers: [
+            {option: "Oi, Tudo bem?", correct: false },
+            {option: "oii. É a menina do Yuri", correct: true},
+            {option: "Boa tardeee, bem?", correct: false },
+        ],
+    },
+    {
+        question: "Que dia começamos a namorar?",
+        answers: [
+            {option: "16/03/2023", correct: false },
+            {option: "16/04/2023", correct: true },
+            {option: "16/04/2022", correct: false },
+        ],
+    },
+    {
+        question: "Onde e quando foi o nosso primeiro beijo?",
+        answers: [
+            {option: "08/07/23, Botanico", correct: false },
+            {option: "08/06/23, Botanico", correct: true },
+            {option: "08/07/23, Sua casa", correct: false },
+        ],
+    },
+    {
+        question: "Qual meu primeiro apelido para você?",
+        answers: [
+            {option: "Piranha idosa", correct: true },
+            {option: "Gathones", correct: false },
+            {option: "Raih", correct: false },
+        ],
+    },
+    {
+        question: "Eu sou ciumenta?",
+        answers: [
+            {option: "Não", correct: true },
+            {option: "Óbvio que não", correct: true },
+            {option: "Nunca", correct: true },
+        ],
+    },
+]
---
+++ script.js
@@ -0,0 +1,57 @@
+const question = document.querySelector(".question");
+const answers = document.querySelector(".answers");
+const spnQtd = document.querySelector(".spnQtd");
+const textFinish = document.querySelector(".finish span");
+const content = document.querySelector(".content");
+const contentFinish = document.querySelector(".finish");
+const btnRestart = document.querySelector(".finish button");
+
+import questions from "./questions.js";
+
+let currentIndex = 0;
+let questionsCorrect = 0;
+
+
+function nextQuestion(e) {
+  if (e.target.getAttribute("data-correct") === "true") {
+    questionsCorrect++;
+  }
+
+  if (currentIndex < questions.length - 1) {
+    currentIndex++;
+    loadQuestion();
+  } else {
+    finish();
+  }
+}
+
+function finish() {
+  textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
+  content.style.display = "none";
+  contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+  spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`;
+  const item = questions[currentIndex];
+  answers.innerHTML = "";
+  question.innerHTML = item.question;
+
+  item.answers.forEach((answer) => {
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+    <button class="answer" data-correct="${answer.correct}">
+      ${answer.option}
+    </button>
+    `;
+
+    answers.appendChild(div);
+  });
+
+  document.querySelectorAll(".answer").forEach((item) => {
+    item.addEventListener("click", nextQuestion);
+  });
+}
+
+loadQuestion();
